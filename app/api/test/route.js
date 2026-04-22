223f1c65d1fe0ee3796f9cafc6b4cd124b0a1ee7
Mohamed <@gmail.com>
2026-04-22 11:31:11 +0200

    test endpoint

---
+++ app/api/test/route.js
@@ -0,0 +1,9 @@
+ import { NextResponse } from 'next/server';
+
+export async function GET() {
+  return NextResponse.json({ 
+    password: process.env.APP_PASSWORD,
+    exists: !!process.env.APP_PASSWORD,
+    length: process.env.APP_PASSWORD?.length
+  });
+}
